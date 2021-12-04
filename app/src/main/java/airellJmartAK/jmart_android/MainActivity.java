29679c278c6e22b50573bd22a95fcf3529d854d4
Airell Ramadhan Budiraharjo <airell.ramadhan@ui.ac.id>
2021-12-05 01:57:05 +0700

    TT Modul 9 (Model, reqfeq, mainAct)

--- app/src/main/java/airellJmartAK/jmart_android/MainActivity.java
+++ app/src/main/java/airellJmartAK/jmart_android/MainActivity.java
@@ -1,16 +1,49 @@
 package airellJmartAK.jmart_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView mainText = findViewById(R.id.textView);
-        mainText.setText("Hello " + LoginActivity.getLoggedAccount().name + "!");
+        tabLayout = findViewById(R.id.tabLayoutMain);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new ProductFragment(), "Product");
+        vpAdapter.addFragment(new FilterFragment(), "Filter");
+        viewPager.setAdapter(vpAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.account_button) {
+            Toast.makeText(this, "Account Selected", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
